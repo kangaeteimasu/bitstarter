3a47eac05f074369c02275a706fe09bb463d9696
kangaeteimasu <tsterling22202@yahoo.com>
2013-08-04 01:26:45 +0000

    edited title in index.html

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.html');
+  var buffer = fs.readFileSync('index2.html');
   response.send(buffer);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
