ae990002c1027e8e9524e8be074631bfe86c1b20
kangaeteimasu <tsterling22202@yahoo.com>
2013-08-06 00:02:00 +0000

    fixed web.js

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.html');
-  response.send(buffer);
+  var html = fs.readFileSync('index.html').toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 8080;
